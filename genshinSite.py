7bc6ad944beadac500eb53c2cf15a614c85606f4
LRiosB <lucas.riosb@gmail.com>
2023-06-07 20:38:04 -0300

    Update genshinSite.py

--- genshinSite.py
+++ genshinSite.py
@@ -1,4 +1,12 @@
 import streamlit as st
 
+hide_streamlit_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            </style>
+            """
+st.markdown(hide_streamlit_style, unsafe_allow_html=True) 
+
 
 st.title("Hello there, gbolinha")
